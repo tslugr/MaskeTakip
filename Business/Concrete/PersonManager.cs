752c8be8a0930d32bf54367970761f8d9b295128
Uğur TAŞLI <ugurtasli@outlook.com>
2021-09-13 19:51:14 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+   public class PersonManager
+    {
+        //Kişi Maskeye başvurmak istiyor
+        public void ApplyForMask(Person person)
+        { 
+
+
+        }
+        public List<Person>GetList()
+        {
+            return null;
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SelamVer(isim:"Uğur");
+            SelamVer();
+            SelamVer();
+            int sonuc = Topla(6, 58);
+            //Diziler /Arrays
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Uğur";
+            ogrenciler[1] = "Nevin";
+            ogrenciler[2] = "Halide";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            Console.ReadLine();
+        }
+        static void SelamVer(string isim="isimsiz")
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla(int sayi1 = 5,int sayi2=10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam : "+sonuc);
+            return sonuc;
+        }
+    }
+    
+}
